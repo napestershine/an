b14de9eddd96077d32c6490084d0803895b2b4c4
root <napestershine@gmail.com>
2016-08-19 20:34:31 +0530

    returning json

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
@@ -13,16 +15,47 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        $notes = [
-            'one',
-            'two',
-            'three'
-        ];
-
         return $this->render('genus/show.html.twig', [
-            'name' => $genusName,
-            'notes' => $notes
+            'name' => $genusName
         ]);
 
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes")
+     * @Method("GET")
+     */
+    public function getNotesAction()
+    {
+        $notes = [
+            [
+                'id' => 1,
+                'username' => 'Shine',
+                'avatarUri' => '/images/leanna.jpg',
+                'note' => 'Octopus asked me a riddle, outsmarted me',
+                'date' => 'Dec. 10, 2016'
+            ],
+            [
+                'id' => 2,
+                'username' => 'Shine',
+                'avatarUri' => '/images/ryan.jpg',
+                'note' => 'I counted 8 legs... as they wrapped around me',
+                'date' => 'Dec. 1, 2016'
+            ],
+            [
+                'id' => 3,
+                'username' => 'Shine',
+                'avatarUri' => '/images/leanna.jpg',
+                'note' => 'Inked!',
+                'date' => 'Aug. 20, 2016'
+            ],
+
+        ];
+
+        $data = [
+            'notes' => $notes
+        ];
+
+        return new JsonResponse($data);
     }
 }
