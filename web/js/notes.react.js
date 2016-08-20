8464a5c1862d0501a2fa4481457676370f2ab257
root <napestershine@gmail.com>
2016-08-20 23:03:04 +0530

    finished react js and first chapter

--- web/js/notes.react.js
+++ web/js/notes.react.js
@@ -1,42 +1,43 @@
 var NoteSection = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             notes: []
         }
     },
 
-    componentDidMount: function() {
+    componentDidMount: function () {
         this.loadNotesFromServer();
         setInterval(this.loadNotesFromServer, 2000);
     },
 
-    loadNotesFromServer: function() {
+    loadNotesFromServer: function () {
         $.ajax({
-            url: '/genus/octopus/notes',
+            url: this.props.url,
             success: function (data) {
                 this.setState({notes: data.notes});
             }.bind(this)
         });
     },
 
-    render: function() {
+    render: function () {
         return (
             <div>
                 <div className="notes-container">
                     <h2 className="notes-header">Notes</h2>
                     <div><i className="fa fa-plus plus-btn"></i></div>
                 </div>
-                <NoteList notes={this.state.notes} />
+                <NoteList notes={this.state.notes}/>
             </div>
         );
     }
 });
 
 var NoteList = React.createClass({
-    render: function() {
-        var noteNodes = this.props.notes.map(function(note) {
+    render: function () {
+        var noteNodes = this.props.notes.map(function (note) {
             return (
-                <NoteBox username={note.username} avatarUri={note.avatarUri} date={note.date} key={note.id}>{note.note}</NoteBox>
+                <NoteBox username={note.username} avatarUri={note.avatarUri} date={note.date}
+                         key={note.id}>{note.note}</NoteBox>
             );
         });
 
@@ -49,11 +50,11 @@ var NoteList = React.createClass({
 });
 
 var NoteBox = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="cd-timeline-block">
                 <div className="cd-timeline-img">
-                    <img src={this.props.avatarUri} className="img-circle" alt="Leanna!" />
+                    <img src={this.props.avatarUri} className="img-circle" alt="Leanna!"/>
                 </div>
                 <div className="cd-timeline-content">
                     <h2><a href="#">{this.props.username}</a></h2>
