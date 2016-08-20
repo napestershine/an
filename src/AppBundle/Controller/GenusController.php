677f254ca748a5e027b8ab25b9b8a2159fecc7db
root <napestershine@gmail.com>
2016-08-20 23:20:37 +0530

    installed knp markdown bundle and implemented it's services

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -15,8 +15,15 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
+
+        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+
+        $funFact = $this->get('markdown.parser')->transform($funFact);
+
+
         return $this->render('genus/show.html.twig', [
-            'name' => $genusName
+            'name' => $genusName,
+            'funFact' => $funFact
         ]);
 
     }
