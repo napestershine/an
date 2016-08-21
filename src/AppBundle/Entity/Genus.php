3f094102af41ea8e6e1fc16f66caebf16645b44e
root <napestershine@gmail.com>
2016-08-21 11:07:35 +0530

    Started Doctrine by createing Genus entity

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 21/8/16
+ * Time: 11:04 AM
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+class Genus
+{
+
+}
