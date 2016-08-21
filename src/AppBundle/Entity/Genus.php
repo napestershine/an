a9f98cb8143ce2dd53301ac6541ca330a8d82305
root <napestershine@gmail.com>
2016-08-21 11:10:59 +0530

    added two properties

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -11,7 +11,21 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
 class Genus
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
 }
