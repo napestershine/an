3884def42f90c1da0400f91a17e37eeb85d4a51f
root <napestershine@gmail.com>
2016-08-21 11:19:38 +0530

    created getter and setter for genus

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -28,4 +28,22 @@ class Genus
      * @ORM\Column(type="string")
      */
     private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
 }
