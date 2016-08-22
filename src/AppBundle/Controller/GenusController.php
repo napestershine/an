c08e5542c7b984268ff8015e9ee01df975225d9e
root <napestershine@gmail.com>
2016-08-22 19:42:37 +0530

    updated recent count

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -94,9 +94,15 @@ class GenusController extends Controller
 
         $this->get('logger')->info('Showing genus: ' . $genusName);
 
+      /*  $recentNotes = $genus->getNotes()->filter(function (GenusNote $note) {
+            return $note->getCreatedAt() > new \DateTime('-3 months');
+        });*/
+
+      $recentNotes = $em->getRepository('AppBundle:GenusNote')->findAllRecentNotesForGenus($genus);
 
         return $this->render('genus/show.html.twig', [
-            'genus' => $genus
+            'genus' => $genus,
+            'recentNoteCount' => count($recentNotes)
         ]);
 
     }
